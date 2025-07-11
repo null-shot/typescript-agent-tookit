e9bc1b6f4d71452ca007bbdf7e92317e4bd4f183
disturbing <hello@xavalabs.com>
2025-07-11 09:40:10 +0800

    Fix lint

--- packages/playground/src/components/chat/chat-message.tsx
+++ packages/playground/src/components/chat/chat-message.tsx
@@ -27,7 +27,7 @@ export interface ChatMessageProps {
 
 export function ChatMessage({
   content,
-  timestamp: _timestamp,
+  timestamp, // eslint-disable-line @typescript-eslint/no-unused-vars
   variant,
   className,
   isThinking = false,
