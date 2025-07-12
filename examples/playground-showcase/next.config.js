c45c7479abd46eca45734d0d64add761ddf083a8
disturbing <hello@xavalabs.com>
2025-07-12 12:06:23 +0800

    ATtempt fix build linting

--- examples/playground-showcase/next.config.js
+++ examples/playground-showcase/next.config.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
@@ -10,13 +8,6 @@ const nextConfig = {
   },
   experimental: {
     esmExternals: 'loose',
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'src'),
-    };
-    return config;
   },
 }
 
