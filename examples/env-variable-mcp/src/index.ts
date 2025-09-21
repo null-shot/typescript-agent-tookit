01a254a62dedc4170fa132b9b70112404e275c53
Allen Wyma <>
2025-09-21 17:47:42 +0800

    env variable example

---
+++ examples/env-variable-mcp/src/index.ts
@@ -0,0 +1,18 @@
+import { EnvVariableMcpServer } from './server';
+// Export the TodoMcpServer class for Durable Object binding
+export { EnvVariableMcpServer };
+
+// Worker entrypoint for handling incoming requests
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const url = new URL(request.url);
+		const sessionIdStr = url.searchParams.get('sessionId');
+		const id = sessionIdStr ? env.EXAMPLE_MCP_SERVER.idFromString(sessionIdStr) : env.EXAMPLE_MCP_SERVER.newUniqueId();
+
+		console.log(`Fetching sessionId: ${sessionIdStr} with id: ${id}`);
+
+		url.searchParams.set('sessionId', id.toString());
+
+		return env.EXAMPLE_MCP_SERVER.get(id).fetch(new Request(url.toString(), request));
+	},
+};
