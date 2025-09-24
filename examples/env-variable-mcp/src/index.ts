0f46c333640dbe327ece5f6c5b33ded03abd45fe
Allen Wyma <>
2025-09-24 12:30:02 +0800

    fix building errors for env var example

--- examples/env-variable-mcp/src/index.ts
+++ examples/env-variable-mcp/src/index.ts
@@ -7,12 +7,12 @@ export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
 		const sessionIdStr = url.searchParams.get('sessionId');
-		const id = sessionIdStr ? env.EXAMPLE_MCP_SERVER.idFromString(sessionIdStr) : env.EXAMPLE_MCP_SERVER.newUniqueId();
+		const id = sessionIdStr ? env.ENV_VARIABLE_MCP_SERVER.idFromString(sessionIdStr) : env.ENV_VARIABLE_MCP_SERVER.newUniqueId();
 
 		console.log(`Fetching sessionId: ${sessionIdStr} with id: ${id}`);
 
 		url.searchParams.set('sessionId', id.toString());
 
-		return env.EXAMPLE_MCP_SERVER.get(id).fetch(new Request(url.toString(), request));
+		return env.ENV_VARIABLE_MCP_SERVER.get(id).fetch(new Request(url.toString(), request));
 	},
 };
