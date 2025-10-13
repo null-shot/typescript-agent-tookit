4b39063a9d37325a534e8715200b781b67696f96
Allen Wyma <allen.wyma@gmail.com>
2025-10-13 17:52:05 +0800

    add in initial version of secret example (#97)
    * add in initial version of secret example
    
    * update tests and package.json
    
    * force add dev.vars file for example
    
    * revert dependent-agent/worker-configuration.d.ts
    
    * udpate README and move cursor rules to top level
    
    ---------
    
    Co-authored-by: Allen Wyma <>

---
+++ examples/secret-mcp/src/tools.ts
@@ -0,0 +1,26 @@
+import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js';
+import { z } from 'zod';
+
+export function setupServerTools(server: McpServer, env: Env) {
+	const secretNumber = parseInt(env.SECRET_NUMBER);
+	// Let's you to guess the secret
+	server.tool(
+		'guess_number',
+		'Tells you if you guessed the number correctly',
+		{
+			guess: z.number().describe('The number you guessed'),
+		},
+		async ({ guess }: { guess: number }) => {
+			const guessedCorrectedly = guess === secretNumber;
+			return {
+				content: [
+					{
+						type: 'text',
+						text: `You guessed ${guess}`,
+					},
+				],
+				correct: guessedCorrectedly,
+			};
+		},
+	);
+}
